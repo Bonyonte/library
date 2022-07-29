de0813fbc68702b2155ad79ff9f733e784c02cf5
Bonyonte <godfrey.magogo@student.moringaschool.comgit config --global user.name Bonyontegit branch -m maingit branch -M maingit remote add origin git@github.com:Bonyonte/ip_1.gitgit push -u origin maingit config --global user.email godfrey.magogo@student.moringaschool.com>
2022-07-29 15:28:47 +0300

    re-design navbar

--- src/components/Book.js
+++ src/components/Book.js
@@ -2,9 +2,16 @@ import React from 'react';
 
 function Book({author, image, }) {
   return (
-    <div>
-        <h4>{author}</h4>
+    <div className="book">
+        <h4 className="author">{author}</h4>
         <img src={image} alt="book" />
+        <div className="row1">
+            <h2>Find your book</h2>
+            <div className="search">
+                <input type="text" placeholder="Enter Your Book Name" />
+                <button><i class="fas fa-search"></i></button>
+            </div>
+        </div>
     </div>
   )
 }
