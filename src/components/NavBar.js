cbc760120ef31a50021528c7b43c37c84e29207e
Bonyonte <godfrey.magogo@student.moringaschool.comgit config --global user.name Bonyontegit branch -m maingit branch -M maingit remote add origin git@github.com:Bonyonte/ip_1.gitgit push -u origin maingit config --global user.email godfrey.magogo@student.moringaschool.com>
2022-07-29 14:33:27 +0300

    add links to navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -6,9 +6,9 @@ import {Link} from 'react-router-dom';
 const NavBar = () => {
   return (
     <div className="navbar">
-        {/* <Link to='/'>Home</Link>
+        <Link to='/'>Home</Link>
         <Link to='/library'>Library</Link>
-        <Link to='/comment'>Comment</Link> */}
+        <Link to='/comment'>Comment</Link>
     </div>
   );
 };
