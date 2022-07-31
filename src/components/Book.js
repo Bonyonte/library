13d9c53f5998170316111ee4d300f6149ad39956
Bonyonte <godfrey.magogo@student.moringaschool.comgit config --global user.name Bonyontegit branch -m maingit branch -M maingit remote add origin git@github.com:Bonyonte/ip_1.gitgit push -u origin maingit config --global user.email godfrey.magogo@student.moringaschool.com>
2022-07-31 13:02:35 +0300

    show details of a book through clicking a detail button

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,22 +1,29 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import '../App';
 
 function Book({author, image, title, description, price, review, source}) {
   const [ actualDetails, setActualDetails] = useState(false);
+
+  const handleClick = () =>{
+    setActualDetails(actual=> !actual)
+  }
 
 //console.log(book);
   return (
     <div className="book">
         <h4 className="author">{author}</h4>
         <img src={image} alt="book" />
-        <button className="bookButton">Details</button>
-        <div>
+        <button className="bookButton" onClick={handleClick}>Details</button>
+        { actualDetails?
+          <div>
           <p>{title}</p>
           <p>{description}</p>
           <p>{price}</p>
           <p>{review}</p>
           <p>{source}</p>
         </div>
+        :null
+        }
     </div>
   )
 }
